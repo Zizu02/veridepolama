a7d8122361b44cfe2a7128e4ee2e0ad9cdd44785
Zizu02 <habibyazar7@gmail.com>
2024-09-14 18:49:48 +0300

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,22 @@
+// src/models/userModel.js
+const { Pool } = require('pg');
+const pool = new Pool();
+
+const getUsers = async () => {
+  const result = await pool.query('SELECT * FROM users');
+  return result.rows;
+};
+
+const createUser = async (user) => {
+  const result = await pool.query(
+    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *',
+    [user.name, user.email]
+  );
+  return result.rows[0];
+};
+
+module.exports = {
+  getUsers,
+  createUser
+};
 
