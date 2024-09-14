35b4faa4203abc59f3826350078407cd9257513b
Zizu02 <habibyazar7@gmail.com>
2024-09-14 20:08:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// Veritabanına bağlantı
-// const db = require('./config/database');
-
 // Kullanıcı oluşturma endpoint'i
 app.post('/create_account', (req, res) => {
     const { email, password, address, phone } = req.body;
-
-    // Kullanıcı veritabanına eklenir
-    // db.query('INSERT INTO users (email, password, address, phone) VALUES (?, ?, ?, ?)', [email, password, address, phone])
-    // .then(() => {
-    //     res.json({ success: true, message: 'Hesap başarıyla oluşturuldu!' });
-    // })
-    // .catch(error => {
-    //     res.status(500).json({ success: false, message: 'Veritabanı hatası', error });
-    // });
 
     // Şimdilik sadece basit bir yanıt döndürüyoruz
     res.json({ success: true, message: 'Hesap başarıyla oluşturuldu!' });
