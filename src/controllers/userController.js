5525e1a7eb0c11bc38ce8b6d07c8bc4349f33531
Zizu02 <habibyazar7@gmail.com>
2024-09-14 18:49:26 +0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,23 @@
+// src/controllers/userController.js
+const userService = require('../services/userService');
+
+// Kullanıcı listeleme
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await userService.getUsers();
+    res.json(users);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// Kullanıcı ekleme
+exports.createUser = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
