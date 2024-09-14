6a45b6c8a13f3fb3694fcae0723ac7a4622f1e4b
Zizu02 <habibyazar7@gmail.com>
2024-09-14 18:51:42 +0300

    Update api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -1,1 +1,12 @@
+// test/api.test.js
+const request = require('supertest');
+const app = require('../index'); // Uygulamanızın başlangıç noktası
+
+describe('GET /', () => {
+  it('should return Hello World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World!');
+  });
+});
 
