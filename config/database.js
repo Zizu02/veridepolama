ede9e4b2a482297585fa150b847383cc0e306256
Zizu02 <habibyazar7@gmail.com>
2024-09-14 19:17:46 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
-// config/database.js
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
-module.exports = pool;
-
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
