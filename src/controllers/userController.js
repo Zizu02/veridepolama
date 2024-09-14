0637cbf9e21b187d7b502bdd009ba3df12aa5564
Zizu02 <habibyazar7@gmail.com>
2024-09-14 19:16:06 +0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,20 @@
-// src/controllers/userController.js
+const express = require('express');
+const router = express.Router();
 const userService = require('../services/userService');
 
-// Kullanıcı listeleme
-exports.getUsers = async (req, res) => {
-  try {
-    const users = await userService.getUsers();
-    res.json(users);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
+// Kullanıcı bilgilerini döndüren endpoint
+router.get('/get_user_info', async (req, res) => {
+    const email = req.query.email;
+    try {
+        const userInfo = await userService.getUserInfoByEmail(email);
+        if (userInfo) {
+            res.json({ success: true, user_info: userInfo });
+        } else {
+            res.json({ success: false, message: 'Kullanıcı bulunamadı' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Sunucu hatası', error });
+    }
+});
 
-// Kullanıcı ekleme
-exports.createUser = async (req, res) => {
-  try {
-    const user = await userService.createUser(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
-
+module.exports = router;
