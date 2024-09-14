db1f0bd7243d081e19dc629bfe0d8b730118ae08
Zizu02 <habibyazar7@gmail.com>
2024-09-14 18:50:04 +0300

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,16 @@
+// src/services/userService.js
+const userModel = require('../models/userModel');
+
+const getUsers = () => {
+  return userModel.getUsers();
+};
+
+const createUser = (user) => {
+  return userModel.createUser(user);
+};
+
+module.exports = {
+  getUsers,
+  createUser
+};
 
