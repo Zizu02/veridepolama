21e96cdf141b05125e09337de7ce3df3d61265f0
Zizu02 <habibyazar7@gmail.com>
2024-09-14 18:51:55 +0300

    Update models.test.js

--- test/models.test.js
+++ test/models.test.js
@@ -1,1 +1,10 @@
+// test/models.test.js
+const userModel = require('../src/models/userModel');
+
+describe('User Model', () => {
+  it('should fetch users', async () => {
+    const users = await userModel.getUsers();
+    expect(Array.isArray(users)).toBe(true);
+  });
+});
 
