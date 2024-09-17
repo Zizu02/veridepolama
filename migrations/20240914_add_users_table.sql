7e1d71c15f0849174087c122b0d718e90bfc6fc7
Zizu02 <habibyazar7@gmail.com>
2024-09-17 21:46:04 +0300

    Update 20240914_add_users_table.sql

--- migrations/20240914_add_users_table.sql
+++ migrations/20240914_add_users_table.sql
@@ -1,8 +1,8 @@
 -- migrations/20240914_add_users_table.sql
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES "user"(id),
-    items TEXT,
+    user_id INTEGER REFERENCES "user"(id) ON DELETE CASCADE,
+    items JSONB,
     total_amount DECIMAL(10, 2),
     status VARCHAR(50) DEFAULT 'onay bekliyor',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
