6978c09d1fe2e24ac51cde45ad76ada14dca5520
Zizu02 <habibyazar7@gmail.com>
2024-09-18 11:26:26 +0300

    Update 20240914_add_users_table.sql

--- migrations/20240914_add_users_table.sql
+++ migrations/20240914_add_users_table.sql
@@ -13,3 +13,17 @@ CREATE TABLE products (
     name VARCHAR(255) UNIQUE NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
+
+INSERT INTO products (name, price) VALUES
+('Adana kebap', 00.00),
+('Kuzu Tandır', 25.00),
+('Mercimek Çorbası', 25.00),
+('Arabaşı Çorbası', 25.00),
+('Salata', 2.00),
+('Mevsim Salata', 3.00),
+('Baklava', 5.00),
+('Trileçe', 4.00),
+('Ayran', 1.00),
+('Limonata', 2.00),
+('Hamburger Menü', 13.00),
+('Pizza Menü', 18.00);
