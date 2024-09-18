98c63c4913bf1e169a7589bfc4c380aa34114abb
Zizu02 <habibyazar7@gmail.com>
2024-09-18 20:43:46 +0300

    Update productsModel.js

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -2,11 +2,26 @@ const db = require('../../config/database');
 
 const ProductsModel = {
     getAllProducts: async () => {
-        const result = await pool.query('SELECT * FROM products');
+        const result = await db.query('SELECT * FROM products');
         return result.rows;
     },
     getProductByName: async (name) => {
-        const result = await pool.query('SELECT * FROM products WHERE name = $1', [name]);
+        const result = await db.query('SELECT * FROM products WHERE name = $1', [name]);
+        return result.rows[0];
+    },
+    // Ürün eklemek için fonksiyon
+    addProduct: async (name, price) => {
+        const result = await db.query('INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *', [name, price]);
+        return result.rows[0];
+    },
+    // Ürün güncellemek için fonksiyon
+    updateProduct: async (id, name, price) => {
+        const result = await db.query('UPDATE products SET name = $1, price = $2 WHERE id = $3 RETURNING *', [name, price, id]);
+        return result.rows[0];
+    },
+    // Ürün silmek için fonksiyon
+    deleteProduct: async (id) => {
+        const result = await db.query('DELETE FROM products WHERE id = $1 RETURNING *', [id]);
         return result.rows[0];
     }
 };
